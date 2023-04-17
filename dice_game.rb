4d704ce0d8028a0e0448a74aff52befa21ca9ab8
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-04-17 16:13:43 +0100

    Changed code a bit, draw puts still doesnt work

--- dice_game.rb
+++ dice_game.rb
@@ -1,19 +1,24 @@
 
-player1 = rand(6) + rand(6)
-player2 = rand(6) + rand(6)
+player1 = rand(1..6) + rand(1..6)
+player2 = rand(1..6) + rand(1..6)
 player1games = 0
 player2games = 0
 
 while true do
-  if player1 > player2
-    puts "Player 1 scored #{player1} and Player 2 scored #{player2}. Player 1 wins!"
-    player1games = player1games + 1
-  elsif  player1 < player2
-    puts "Player 2 scored #{player2} and Player 1 scored #{player1}. Player 2 wins!"
-    player2games = player2games + 1
-  elsif player1 == player2
-    puts " Player 1 scored #{player1} and Player 2 scored #{player2}. It's a draw!"
-  if player1games >= 2 || player2games >= 2
-    break
+  player1
+  player2
+  player1games += 1 if player1 > player2
+  player2games += 1 if player2 > player1
+  break if player1games == 2 || player2games == 2  
 end
+
+    
+if player1 > player2
+  puts "Player 1 scored #{player1} and Player 2 scored #{player2}. Player 1 wins!"
+elsif player1 < player2
+  puts "Player 2 scored #{player2} and Player 1 scored #{player1}. Player 2 wins!"
+elsif player1 == player2
+  puts " Player 1 scored #{player1} and Player 2 scored #{player2}. It's a draw!"
+end
+
 
