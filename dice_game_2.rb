337bb02acb21271131d49ffc9ba61dcc80f728ab
Joe Hannis <joe_hannis@hotmail.com>
2023-04-17 17:52:28 +0100

    dice game revision

--- dice_game_2.rb
+++ dice_game_2.rb
@@ -13,14 +13,3 @@ while true do
   player2games += 1 if player2 > player1 && puts "Player 2 wins!"
   break if player1games == 2 || player2games == 2  
 end
-
-    
-if player1 > player2
-  puts "Player 1 wins!"
-elsif player1 < player2
-  puts "Player 2 wins!"
-elsif player1 == player2
-  puts "It's a draw!"
-end
-
-
