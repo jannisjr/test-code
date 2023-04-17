8066070900f2563307f5bb72ae6262522ef1eccd
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-04-17 16:28:11 +0100

    Total count still wont work

--- dice_game.rb
+++ dice_game.rb
@@ -2,12 +2,13 @@ player1games = 0
 player2games = 0
 
 def dice
-  return rand(1..12), rand(1..12)
+  return rand(1..6), rand(1..6)
 end
 
 while true do
   player1, player2 = dice
   puts "P1: #{player1} |||| P2: #{player2}"
+  puts "----------------------------------"
   player1games += 1 if player1 > player2
   player2games += 1 if player2 > player1
   break if player1games == 2 || player2games == 2  
@@ -15,11 +16,11 @@ end
 
     
 if player1 > player2
-  puts "Player 1 scored #{player1} and Player 2 scored #{player2}. Player 1 wins!"
+  puts "Player 1 wins!"
 elsif player1 < player2
-  puts "Player 2 scored #{player2} and Player 1 scored #{player1}. Player 2 wins!"
+  puts "Player 2 wins!"
 elsif player1 == player2
-  puts " Player 1 scored #{player1} and Player 2 scored #{player2}. It's a draw!"
+  puts "It's a draw!"
 end
 
 
