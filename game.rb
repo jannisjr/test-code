7ff117dc4a2d8c6ecfec623c72b6bbdad459d37e
Joe Hannis <joe_hannis@hotmail.com>
2023-04-19 10:14:45 +0100

    turn based game

---
+++ game.rb
@@ -0,0 +1,26 @@
+player1 = "Joe"
+left = String.new("#{player1} died, there's a goblin!")
+right = String.new("#{player1} died, there's a werewolf!")
+forward = String.new("#{player1} lives to fight another turn!")
+turns = 0
+
+while true do
+  if turns == 2
+    puts "#{player1} Wins!"
+    break
+  end
+  
+  puts " Which direction would you like to move #{player1}? Left, right or forward?"
+  move = gets.chomp
+
+  if move == "left"
+    puts left 
+    turns = turns + 1
+  elsif move == "right"
+    puts right 
+    turns = turns + 1
+  elsif move == "forward"
+    puts forward 
+    turns = turns + 1
+  end
+end
